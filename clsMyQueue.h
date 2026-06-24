f005ab85419cd289b459b3fc5bc2c70fc0fc2593
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-06-24 23:38:22 +0300

    Implemented Print, Push, Pop, Front, Back, and Size methods

--- clsMyQueue.h
+++ clsMyQueue.h
@@ -1,1 +1,37 @@
 #pragma once
+#include"clsDblLinkedList.h"
+
+template <class T>
+
+class clsMyQueue {
+
+private:
+	clsDblLinkedList<T> _MyList;
+public:
+
+	void Print() {
+		_MyList.PrintList();
+	}
+
+	void push(T value) {
+		_MyList.InsertAtEnd(value);
+	}
+
+	void pop() {
+		_MyList.DeleteFirstNode();
+	}
+
+	T front() {
+		return _MyList.GetItem(0);
+	}
+
+	T back() {
+		return _MyList.GetItem(_MyList.Size() - 1);
+	}
+
+	int size() {
+		return _MyList.Size();
+	}
+
+
+};
