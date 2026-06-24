488f6af3cbecba0b0d3630d44d9afd68f5a701b9
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-06-24 23:40:53 +0300

    Implemented GetItem, Reverse, UpdateItem, InsertAfter, InsertAtFront, InsertAtBack, and Clear methods

--- clsMyQueue.h
+++ clsMyQueue.h
@@ -33,5 +33,33 @@ public:
 		return _MyList.Size();
 	}
 
+	T GetItem(int index) {
+		return _MyList.GetItem(index);
+	}
+
+	void Reverse() {
+		_MyList.Reverse();
+	}
+
+	bool UpdateItem(int index, T newValue) {
+		return _MyList.UpdateItem(index, newValue);
+	}
+
+	bool InsertAfter(int index, T value) {
+
+		return _MyList.InsertAfter(index, value);
+	}
+
+	void InsertAtFront(T value) {
+		_MyList.InsertAtBeginning(value);
+	}
+
+	void InsertAtBack(T value) {
+		_MyList.InsertAtEnd(value);
+	}
+
+	void Clear() {
+		_MyList.Clear();
+	}
 
 };
