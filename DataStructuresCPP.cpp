739223a725d1bbb04ca5494bfcb6eb69d1ca31db
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-06-25 13:46:17 +0300

    Tested SetItem, IsEmpty, Size, and Print methods

--- DataStructuresCPP.cpp
+++ DataStructuresCPP.cpp
@@ -2,54 +2,25 @@
 #include "clsDblLinkedList.h"
 #include"clsMyQueue.h"
 #include"clsMyStack.h"
+#include"clsDynamicArray.h"
 
 using namespace std;
 
 int main()
 {
-	clsMyStack<int> MyStack;
-	MyStack.Push(10);
-	MyStack.Push(20);
-	MyStack.Push(30);
-	MyStack.Push(40);
-	MyStack.Push(50);
+	clsDynamicArray<int> arr(5);
+	arr.SetItem(0, 10);
+	arr.SetItem(1, 20);
+	arr.SetItem(2, 30);
+	arr.SetItem(3, 40);
+	arr.SetItem(4, 50);
+	arr.Print();
 
-	MyStack.Print();
-	cout << "Stack size: " << MyStack.size() << endl;
-	cout << "\nStack Top: " << MyStack.Top() << endl;
-	cout << "\nStack Bottom: " << MyStack.Bottom() << endl;
+	cout << "\nis empty: " << arr.IsEmpty() << endl;
 
-	MyStack.pop();
-	cout << "\nStack after Pop: ";
-	MyStack.Print();
+	cout << "size: " << arr.size() << endl;
 
-	MyStack.Reverse();
-	cout << "\nStack after Reverse: ";
-	MyStack.Print();
 
-	MyStack.UpdateItem(2, 25);
-	cout << "\nStack after UpdateItem(2, 25): ";
-	MyStack.Print();
-
-	MyStack.InsertAfter(2, 15);
-	cout << "\nStack after InsertAfter(2, 15): ";
-	MyStack.Print();
-
-	MyStack.InsertAtFront(5);
-	cout << "\nStack after InsertAtFront(5): ";
-	MyStack.Print();
-
-	MyStack.InsertAtBack(55);
-	cout << "\nStack after InsertAtBack(55): ";
-	MyStack.Print();
-
-	MyStack.pop();
-	cout << "\nStack after Pop: ";
-	MyStack.Print();
-
-	MyStack.Clear();
-	cout << "\nStack after Clear: ";
-	MyStack.Print();
 
 	cout << endl;
 	return 0;
