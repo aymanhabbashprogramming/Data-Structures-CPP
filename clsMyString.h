8a137bc755563c4e634f0315f2596502a3b44509
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-06-25 14:26:22 +0300

    Implemented Set, Get, Undo, and Redo methods

--- clsMyString.h
+++ clsMyString.h
@@ -1,1 +1,46 @@
 #pragma once
+#include<string>
+#include <stack>
+
+using namespace std;
+
+class clsMyString
+{
+private:
+	string _value;
+	stack <string> _undo;
+	stack <string> _redo;
+
+public:
+
+	void Set(string value) {
+		_undo.push(_value);
+		_value = value;
+	}
+
+	string Get() {
+		return _value;
+	}
+
+	__declspec(property(get = Get, put = Set)) string Value;
+
+	void Undo() {
+		if (!_undo.empty())
+		{
+			_redo.push(_value);
+			_value = _undo.top();
+			_undo.pop();
+
+		}
+	}
+
+	void Redo() {
+		if (!_redo.empty())
+		{
+			_undo.push(_value);
+			_value = _redo.top();
+			_redo.pop();
+		}
+	}
+
+};
