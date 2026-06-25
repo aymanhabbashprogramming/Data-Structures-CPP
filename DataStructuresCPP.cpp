047995b713052f6126a60ec60345062689b0b7b0
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-06-25 14:00:34 +0300

    Tested DeleteFirstItem and DeleteLastItem methods

--- DataStructuresCPP.cpp
+++ DataStructuresCPP.cpp
@@ -16,9 +16,15 @@ int main()
 	arr.SetItem(4, 50);
 	arr.PrintList();
 
-	arr.DeleteItemAt(0);
-	cout << "\nArray after deleting item at index 0: ";
+	arr.DeleteFirstItem();
+	cout << "\n\nArray after deleting first item: ";
 	arr.PrintList();
+	cout << "\nAfter deleting first item, size: " << arr.size() << endl;
+
+	arr.DeleteLastItem();
+	cout << "\nArray after deleting last item: ";
+	arr.PrintList();
+	cout << "\nAfter deleting last item, size: " << arr.size() << endl;
 
 
 	cout << endl;
