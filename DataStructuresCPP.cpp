e479aac03befe215a6adee94c414d9cbde24d33b
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-06-25 14:11:52 +0300

    Tested InsertAtBeginning, InsertBefore, InsertAfter, and InsertAtEnd methods

--- DataStructuresCPP.cpp
+++ DataStructuresCPP.cpp
@@ -1,29 +1,54 @@
 #include <iostream>
 #include "clsDblLinkedList.h"
-#include"clsMyQueue.h"
-#include"clsMyStack.h"
-#include"clsDynamicArray.h"
+#include "clsMyQueue.h"
+#include "clsMyStack.h"
+#include "clsDynamicArray.h"
 
 using namespace std;
 
 int main()
 {
-	clsDynamicArray<int> arr(5);
-	arr.SetItem(0, 10);
-	arr.SetItem(1, 20);
-	arr.SetItem(2, 30);
-	arr.SetItem(3, 40);
-	arr.SetItem(4, 50);
-	arr.PrintList();
+    clsDynamicArray<int> arr(7);
 
-	cout << "\nsize: " << arr.size() << endl;
+    arr.SetItem(0, 10);
+    arr.SetItem(1, 20);
+    arr.SetItem(2, 30);
+    arr.SetItem(3, 40);
+    arr.SetItem(4, 50);
+    arr.SetItem(5, 60);
+    arr.SetItem(6, 70);
 
-	arr.InsertAt(2, 35);
-	cout << "After InsertAt(2, 35): ";
+    cout << "Initial Array:\n";
+    arr.PrintList();
 
-	arr.PrintList();
-	cout << "\nsize: " << arr.size() << endl;
+    cout << "\n\nIs Empty: " << arr.IsEmpty() << endl;
+    cout << "Size: " << arr.size() << endl;
 
-	cout << endl;
-	return 0;
+    arr.InsertAtBeginning(35);
+
+    cout << "\n\nAfter InsertAtBeginning(35):\n";
+    cout << "Size: " << arr.size() << endl;
+    arr.PrintList();
+
+    arr.InsertBefore(0, 25);
+
+    cout << "\n\nAfter InsertBefore(0, 25):\n";
+    cout << "Size: " << arr.size() << endl;
+    arr.PrintList();
+
+    arr.InsertAfter(5, 45);
+
+    cout << "\n\nAfter InsertAfter(5, 45):\n";
+    cout << "Size: " << arr.size() << endl;
+    arr.PrintList();
+
+    arr.InsertAtEnd(75);
+
+    cout << "\n\nAfter InsertAtEnd(75):\n";
+    cout << "Size: " << arr.size() << endl;
+    arr.PrintList();
+
+    cout << endl;
+
+    return 0;
 }
